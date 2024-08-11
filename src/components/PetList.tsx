5397cf2303927c116320a9e3c9366d67751af2a3
haideralwaqfi <haideralwaqfi@gmail.com>
2024-08-11 23:05:25 +0300

    adding pet type

--- src/components/PetList.tsx
+++ src/components/PetList.tsx
@@ -1,11 +1,14 @@
+import { Pet } from "@/lib/types";
 import Image from "next/image";
 import React from "react";
-
-export default function PetList({ pets }) {
+type PetListProps = {
+  pets: Pet[];
+};
+export default function PetList({ pets }: PetListProps) {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       {pets.map((pet) => (
-        <li key={pet.key}>
+        <li key={pet.id}>
           <button className="flex h-[70px] items-center w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#eff1f2] focus:bg-[#eff1f2] transition">
             <Image
               src={pet.imageUrl}
