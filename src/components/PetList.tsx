940ae7c9a73f557a15194c1613fa8d22f4cbe0c8
haideralwaqfi <haideralwaqfi@gmail.com>
2024-08-12 12:24:13 +0300

    tailwind config extend

--- src/components/PetList.tsx
+++ src/components/PetList.tsx
@@ -8,7 +8,7 @@ import React from "react";
 export default function PetList() {
   const { pets, handleChangeSelectedPetId, selectedPetId } = usePetContext();
   return (
-    <ul className="bg-white border-b border-black/[0.08]">
+    <ul className="bg-white border-b border-light">
       {pets.map((pet) => (
         <li key={pet.id}>
           <button
